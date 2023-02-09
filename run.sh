b9c24f3243f126a90c4549048771b3a518574b58
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-02-09 18:31:46 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ ls
 sleep 1
 
 sudo docker build --no-cache -t altlokalt-mantine-production .
-sudo docker run -p 491:3000 --name altlokalt -it altlokalt-mantine-production:latest &
+sudo docker run -p 491:3000 -d --name altlokalt -it altlokalt-mantine-production:latest &
