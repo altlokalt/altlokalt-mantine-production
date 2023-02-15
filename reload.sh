09eef6dfdfb5ef72ec0a6277163973cbe9cee3e1
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-02-15 02:44:34 +0100

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -10,7 +10,7 @@ cd ..
 sudo rm -rf altlokalt-mantine-production/
 ls
 sleep 5
-sudo git clone https://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@github.com/valiantlynx/altlokalt-mantine-production.git
+sudo git clone https://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@github.com/valiantlynx/altlokalt-mantine-production.git
 cd altlokalt-mantine-production/
 ls
 sleep 3
